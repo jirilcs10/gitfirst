7fb3caae6a707e9a54fce899d003bcfe917cf634
jirilcs10 <jirilcherianshaju@gmail.com>
2023-10-11 19:34:19 +0530

    second(push to same repo)

---
+++ demo.js
@@ -0,0 +1,16 @@
+
+// console.dir(document);
+//  console.log(document.domain);
+//  console.log(document.URL);
+//  console.log(document.title);
+//  console.log(document.forms);
+//console.log(document.getElementById("header-title"));
+//var headt=document.getElementById("header-title");
+var headm=document.getElementById("main-header");
+
+//console.log(head);
+headm.style.border="black 3px solid";
+
+var tt=document.getElementsByClassName("title");
+tt[0].style.fontWeight="bold";
+//tt[0].innerHTML="<b>Add Items</b>";
